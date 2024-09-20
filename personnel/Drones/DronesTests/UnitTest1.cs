7946ba69d11d3b29764aee0bf80014cf9936fc91
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-09-20 12:00:46 +0200

    feat(Drones): New test Drone_Update_ReducesBatteryCharge

--- personnel/Drones/DronesTests/UnitTest1.cs
+++ personnel/Drones/DronesTests/UnitTest1.cs
@@ -17,5 +17,20 @@ namespace DronesTest
             // Assert
             Assert.AreEqual(1000, drone.Charge, "La charge du drone doit �tre de 1000");
         }
+
+        [TestMethod]
+        public void Drone_Update_ReducesBatteryCharge()
+        {
+            // Arrange
+            Drone drone = new Drone("test", new Position(50, 50));
+
+            // Action
+            int initialCharge = drone.Charge;
+            drone.Update(1);
+
+            // Assert
+            Assert.IsTrue(initialCharge > drone.Charge, "La charge du drone doit r�duire apr�s une update");
+        }
+        }
     }
 }
