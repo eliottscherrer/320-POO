f8a8e9991a7cd055427afaeab73227bb609c6ef0
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-04 11:24:51 +0200

    refactor(Box): Change magic numbers to consts

--- personnel/Drones/Drones/Model/Box.cs
+++ personnel/Drones/Drones/Model/Box.cs
@@ -4,17 +4,26 @@ namespace Drones
 {
     public partial class Box
     {
+        ////////////////////////////////// [ CONSTS ] //////////////////////////////////
+
+        private const int MIN_WEIGHT = 5;
+        private const int MAX_WEIGHT = 10;
+
+        /////////////////////////////////// [ VARS ] ///////////////////////////////////
+
         private static int _idCounter = 0;
-        private static Color[] _colors = { Color.Red, Color.Yellow, Color.Blue, Color.Brown, Color.Orange };
+        private static readonly Color[] _colors = { Color.Red, Color.Yellow, Color.Blue, Color.Brown, Color.Orange };
 
         public readonly int ID;                         // Unique identifier
         public double Weight { get; private set; }      // In kilos
         public Color Color { get; private set; }        // Red, Yellow, Blue, Brown or Orange
 
+        ///////////////////////////////////////////////////////////////////////////////
+
         public Box()
         {
-            Weight = GlobalHelpers.Rand.Next(5, 11);                // Random weight between 5 and 10 kilos.
             ID = _idCounter++;                                                  // Ids start at 0
+            Weight = GlobalHelpers.Rand.Next(MIN_WEIGHT, MAX_WEIGHT + 1);       // Random weight between 5 and 10 kilos.
             Color = _colors[GlobalHelpers.Rand.Next(_colors.Length)];
         }
 
