d40c440735e283d55b0f43890c95ac12a30206ba
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-04 10:29:54 +0200

    feat(Factory): Added a unique identifier to factories

--- personnel/Drones/Drones/Model/Factory.cs
+++ personnel/Drones/Drones/Model/Factory.cs
@@ -1,17 +1,35 @@
-﻿namespace Drones
+﻿using Drones.Helpers;
+using System;
+
+namespace Drones
 {
     public partial class Factory : Building
     {
+        ////////////////////////////////// [ CONSTS ] //////////////////////////////////
+        
+        
+
+        /////////////////////////////////// [ VARS ] ///////////////////////////////////
+        
+        private static int _factoryCounter = 0;
+        public readonly int ID;                                     // Unique identifier
         public double PowerConsumption { get; private set; }        // Nombre de KwH/jour
+
+        ///////////////////////////////////////////////////////////////////////////////
+        
         public Factory(int x, int y, int size, Color color, double powerConsumption) : base(x, y, size, size, color)
         {
+            ID = _factoryCounter++;
             PowerConsumption = powerConsumption;
+
             Print();
         }
 
         public Factory(Position position, int size, Color color, double powerConsumption) : base(position, size, size, color)
         {
+            ID = _factoryCounter++;
             PowerConsumption = powerConsumption;
+
             Print();
         }
     }
