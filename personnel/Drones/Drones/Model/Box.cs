244d86f903bcba8339b5f8a3e32597529d19d258
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2024-10-04 11:15:54 +0200

    typo(Box): Ids start at 0, not 1

--- personnel/Drones/Drones/Model/Box.cs
+++ personnel/Drones/Drones/Model/Box.cs
@@ -7,14 +7,14 @@ namespace Drones
         private static int _idCounter = 0;
         private static Color[] _colors = { Color.Red, Color.Yellow, Color.Blue, Color.Brown, Color.Orange };
 
-        public int ID { get; private set; }             // Unique identifier
+        public readonly int ID;                         // Unique identifier
         public double Weight { get; private set; }      // In kilos
         public Color Color { get; private set; }        // Red, Yellow, Blue, Brown or Orange
 
         public Box()
         {
-            ID = _idCounter++;                                      // Ids start at 0
             Weight = GlobalHelpers.Rand.Next(5, 11);                // Random weight between 5 and 10 kilos.
+            ID = _idCounter++;                                                  // Ids start at 0
             Color = _colors[GlobalHelpers.Rand.Next(_colors.Length)];
         }
 
